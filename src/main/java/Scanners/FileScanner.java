768cf45541229254d9c72ae96dd46f6e670febde
Ricardo Simões <rfns0303@gmail.com>
2023-09-30 15:46:56 +0100

    Add FileScanner Interface Add package for exercices

---
+++ src/main/java/Scanners/FileScanner.java
@@ -0,0 +1,25 @@
+package Scanners;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public interface FileScanner {
+    static List<String[]> lerCSV(String fileName) {
+        List<String[]> lines = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                lines.add(data);
+            }
+        } catch (IOException e) {
+            System.err.println("Ficherio em formato inválido, tente o formato CSV " + e.getMessage());
+        }
+
+        return lines;
+    }
+}
