50dafcd9cd16f0228499a111c42d74ebc49b1209
Ricardo Simões <rfns0303@gmail.com>
2023-09-30 15:48:09 +0100

    Add Comment for FileScanner

--- src/main/java/Scanners/FileScanner.java
+++ src/main/java/Scanners/FileScanner.java
@@ -6,17 +6,30 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Interface que permite ler ficheiros CSV
+ */
 public interface FileScanner {
+
+    /**
+     * Método que lê um ficheiro CSV e retorna uma lista de arrays de Strings
+     * @param fileName nome do ficheiro CSV
+     * @return lista de arrays de Strings
+     */
     static List<String[]> lerCSV(String fileName) {
+
+        // Lista de arrays de Strings
         List<String[]> lines = new ArrayList<>();
 
+
+        // Tenta ler o ficheiro CSV
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
                 lines.add(data);
             }
-        } catch (IOException e) {
+        } catch (IOException e) { // Caso não consiga ler o ficheiro
             System.err.println("Ficherio em formato inválido, tente o formato CSV " + e.getMessage());
         }
 
